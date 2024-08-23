3119bf66b0529ceaf685615932064d6b757cf911
Erik S. Lopes <44042780+ErikmtLopes@users.noreply.github.com>
2024-08-22 21:16:55 -0300

    Add files via upload

---
+++ TDE_Aula03/Program.cs
@@ -0,0 +1,13 @@
+﻿Console.WriteLine("Bem vindo a calculadora. Aqui vamos apresentar o resultado das quatro operações básicas");
+Console.WriteLine("Digite o primeiro número: ");
+double num1 = Convert.ToDouble(Console.ReadLine());
+Console.WriteLine("Digite o segundo número: ");
+double num2 = Convert.ToDouble(Console.ReadLine());
+
+Console.WriteLine("O resultado da soma é: " + (num1+num2));
+
+Console.WriteLine($"O resultado da subtração é:" {num1 - num2});
+
+Console.WriteLine("O resultado da multiplicação é {0}", num1*num2);
+
+Console.WriteLine("O resultado da multiplicação é {0}", num1/num2);
