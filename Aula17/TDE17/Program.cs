b3cd969c7c6d097268962639804493fdafda1391
Erik S. Lopes <44042780+ErikmtLopes@users.noreply.github.com>
2024-10-31 21:29:45 -0300

    Add files via upload

---
+++ Aula17/TDE17/Program.cs
@@ -0,0 +1,8 @@
+﻿Console.WriteLine(ConversorTemperatura.CelsiusParaFahrenheit(16));
+Console.WriteLine(ConversorTemperatura.FahrenheitParaCelsius(16));
+
+Configuracoes.NomeAplicacao = "teste";
+Configuracoes.Versao = 1;
+
+Console.WriteLine($"Nome Aplicação: {Configuracoes.NomeAplicacao}, Versão: {Configuracoes.Versao}");
+
